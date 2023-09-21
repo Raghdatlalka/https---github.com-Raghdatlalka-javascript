26d5f3b36206e759e3acebd1bd5cb83d79a5b6f9
Raghdatlalka <ragda.altlalka37@gmail.com>
2023-09-21 13:42:57 +0300

    q1

---
+++ main.js
@@ -0,0 +1,38 @@
+
+//string
+console.log('raghda');
+//number
+let x2 = 34;
+console.log(x2);
+//object
+const person = {
+  firstName : "raghda",
+  lastName  : "tlalka",
+  age     : 24,
+  eyeColor  : "brown"
+};
+
+console.log(person.firstName + " is " + person.age + " years old.");
+//boolean
+
+let x = 5;
+let y = 5;
+let z = 6;
+console.log((x == y) 
+ + (x == z));
+
+ //null
+ var a;
+ a=null;
+console.log(a);
+
+//undefined
+let car;
+console.log(car);
+
+//symbol
+
+let symbol1 = Symbol('symbol2');
+let symbol2 = Symbol('symbol2');
+console.log(symbol1===symbol2);
+
